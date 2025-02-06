cf7b94662ee291345ce115bfa2b603ab3a206625
Alex Zloy <usvplus@gmail.com>
2025-02-06 18:35:07 +0200

    grid

--- src/main.js
+++ src/main.js
@@ -1,35 +1,7 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+import Accordion from 'accordion-js';
 
-
-new Accordion(".accordion-container", {
+new Accordion('.faq-list', {
   duration: 900,
   showMultiple: true,
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  }
+  elementClass: `faq-item`,
 });
-
-
-
-
-
-// document.querySelectorAll('.faq-accordion-header').forEach(header => {
-//   header.addEventListener('click', () => {
-//     const content = header.nextElementSibling;
-//     const icon = header.querySelector('.faq-icon');
-//     const isVisible = content.style.display === 'block';
-
-//     // Закрити всі контенти
-//     document
-//       .querySelectorAll('.faq-accordion-content')
-//       .forEach(c => (c.style.display = 'none'));
-//     document.querySelectorAll('.faq-icon').forEach(i => (i.textContent = '+')); // скинути іконки
-
-//     // Відкрити обране
-//     if (!isVisible) {
-//       content.style.display = 'block';
-//       icon.textContent = '#'; // змінити іконку на "#"
-//     }
-//   });
-// });
